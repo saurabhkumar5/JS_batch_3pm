7e023d474d59da50818b6769c2d8af21403c1769
saurabh kumar <saurabhkumar9493@gmail.com>
2024-02-15 20:56:23 +0530

    first commit

---
+++ class1_keyword.js
@@ -0,0 +1,111 @@
+// console.log('simran','amandeep')
+
+
+
+        // a = 10;
+        // a = 15
+        // b= 20;
+        // b=30;
+
+        // a*b=200
+
+        // var a = 10;
+        // var b = 20;
+
+        // console.log(a);   console.log(b);   console.log(a);  console.log(a)
+
+        // console.log("komal");
+        // console.log("mahi");
+       
+        
+       
+
+        // var SIMRAN = 'simran';
+
+        //  console.log(SIMRAN);
+
+        // var mo2mo1 = "kullu";
+        // console.log(mo2mo1)
+
+
+        // var _saurabh = 1;
+        // console.log(_saurabh)
+
+
+        // var $aniket = 45;
+        // console.log($aniket)
+
+
+        // var firstName = "saurabh";
+        // var first_name = "kumar";
+
+
+        // let,const
+
+
+        // let a = "amandeep";
+        // console.log(a);
+
+        // const b = 10;
+        //  console.log(b)
+
+
+        // var a = 10;
+        // var a = 20;
+        // a = 30;
+
+        // console.log(a);
+
+
+
+        // let a = 10;
+        // //   let a = 20
+        // a = 30
+        // console.log(a)
+
+
+
+        // const a = 10;
+        // // const a = 20;
+        // // a = 30;
+        // console.log(a)
+
+
+
+//    1.     var a = "sugamano";
+//         let b = "sugam";
+//         const c = "nanni";
+
+//             {
+//                 // console.log(a)
+//                 // console.log(b)
+//                 // console.log(c)
+//             }
+
+
+// 2.        
+        //     const c = "mohali"
+
+             {
+                // var a = "hola";
+                // let b = "kullu";
+                // console.log(b)
+
+                // const c = "manali";
+             }
+             
+        //      let b = "jammu"
+        //        const c = "spiti"
+        //      console.log(b)
+                // console.log(c)
+
+                // var a = "delhi"
+                // a = "kerala"
+                // console.log(a)
+
+
+
+
+                
+
+
---
+++ class2_dataType.js
@@ -0,0 +1,80 @@
+
+//data Types
+
+// 1.Number
+// 2.String
+// 3.Boolean
+// 4.Undefined
+// 5.Null
+// 6.BigInt
+
+// 7.Object
+// 8. Array
+// 8.Symbols
+
+ 
+// 1.Number
+   // let a = 8.5;
+    // let b = -12;
+    // console.log(a+b)
+
+    // let age = 10;
+        // age = age+10;
+        // age+=20;
+
+        // age-=10 // age = age-10
+
+        // age*=10;
+        // age/=10;
+
+        // age%=10;   
+
+    
+        // console.log(age)
+
+
+        // let age = 10;
+        //     //  age+=1;
+        //     // age++;
+            
+        //   age+=2  ;
+        //      console.log(age)
+
+
+
+
+        // let a = 10;
+        // let value1 = ++a;  //pre-increment a+1
+        // // console.log(a);
+        // console.log(value1);  
+
+
+//  2.       // let a = 10;
+        // let b = --a;  //pre-dec
+        // console.log(a)
+        // console.log(b)
+
+
+// 3.   
+      
+// let age = 12;
+// let value = ++age;
+// // console.log(age);
+// console.log(age)
+// console.log(age)
+// console.log(age)
+
+
+// template litrals  
+
+// let age = 10;
+// let age1 = 12;
+
+// console.log(`my age is ${age} and tushar age is  ${age1}`)
+
+
+
+// let a = 100.1;
+// let b = 100;
+
+// console.log(a>=b)
