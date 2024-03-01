1acefb7d0886149c7689f91cb85a2079b168fd44
saurabh kumar <saurabhkumar9493@gmail.com>
2024-03-01 16:13:24 +0530

    array

--- class5_string.js
+++ class5_string.js
@@ -1,11 +1,187 @@
 
-let str = "ku mkum";  //index strart from zero
+// let str = "ku mkum";  //index strart from zero
 
 
-// let str1 = "10";
+// // let str1 = "10";
 
-// console.log(typeof(str1))
+// // console.log(typeof(str1))
 
-console.log(str.length);
+// console.log(str.length);
 
 // console.log(str[2]);
+
+
+
+// let str1 = 'manmeet_kour';
+// let str2 = "mannat";
+
+// // console.log(str1+"       "+ str2);
+
+
+
+// // 2. concat()
+
+// let str3 = str1.concat(str1)
+// console.log(str3);
+
+
+// let a = 12;
+// let b = +'komal';
+// let c = (a+b)
+
+// console.log(typeof(b))
+
+
+// 3.indexOf()
+
+// let a = 'tushar';
+
+// let a = "most  welcome koko"
+
+// console.log(a.indexOf('o',2));
+// console.log(a.indexOf('wel ko'));
+
+// console.log(a[6])
+
+
+
+// 4.replace(), replaceAll()
+
+// let greet = "most Welcome ";
+
+// console.log(greet.replaceAll('o','handsome'))
+
+
+
+// 5 search()
+
+//  let greet = "hello how are you";
+
+//  console.log(greet.search('howw'))
+
+
+
+// 6.toUppercase()
+
+// let naam = "jagmohan";
+
+// console.log(naam.toUpperCase());
+
+// let naam = "KOKO";
+
+// console.log(naam.toLocaleLowerCase())
+
+
+// 7.includes()
+
+// let str = "welcome back";
+
+// console.log(str.includes('back'))
+
+
+// 8.*** trim()
+
+// let a = "         welcome           ";
+// console.log(a.trim())
+
+
+//9. startsWith()
+
+// let a = "welcome back";
+// console.log(a.startsWith('welcome'))
+
+
+
+
+// let str = "rojgar mela";
+
+//      if(str.length===7){
+//         console.log("you are not berjgar")
+//      }else{
+//         console.log("its ok")
+//      }
+
+
+// 10. charAt()
+
+// let str = "jagmohan";
+
+// console.log(str.charAt(2));
+
+
+// 11.split()
+
+// let place = "kullu manali shimla";
+// // console.log(place.split());
+// // console.log(place.split(''));
+// console.log(place.split(' '));
+
+
+// let a = "hello,how are, you";
+
+// console.log(a.split(' '))
+
+// console.log("most welcome".split(' '));
+
+
+// 12. endsWith()
+
+// let a = "most welcom";
+
+// console.log(a.endsWith('m'))
+
+//   let a = "nepala";
+
+//       if(a.endsWith('a')){
+//         console.log('you can visit in my country')
+//       }else{
+//         console.log("you cant visit in my country")
+//       }
+
+
+// 13.charCodeAt()
+      
+    //   let str = ".";
+    //   console.log(str.charCodeAt());
+
+
+
+    // 14. repeat()
+
+    // let a = "welcome  ";
+    // console.log(a.repeat(3))
+
+
+
+    // 15. substring()
+
+    // let a = "welcome";
+    // console.log(a.substring(3,6))
+    // console.log(a.substring(-6,-3))
+
+
+    // 16. slice()
+
+    // let str = "I love coding";
+
+    // console.log(str.slice(2,8));
+    // console.log(str.slice(-8,-2));
+
+    // 17.padStart()
+
+    // let a = '5';
+    // console.log(a.padStart(7,'2'))
+
+    // 18. padEnd()
+    //   H.W.
+    // 19. trimStart()
+    // 20. trimEnd()
+
+
+
+
+    let a = "raman";
+    let b = "aman";
+    let c = "kullu";
+
+    // ab start hoti h arrray
---
+++ class6_array.js
@@ -0,0 +1,167 @@
+
+
+// let arr = ["aman","kullu","manali",12,true];
+
+
+// console.log(typeof(arr))
+
+// console.log(arr.length);
+
+// console.log(arr)
+
+// let a = [1,0,"ram","kullu"]
+
+// console.log(a.length);
+
+// 1. at()
+
+// let b = [1,5,8,9,6]
+// console.log(b[-3]);
+// console.log(b.at(-3)); //-ve and +ve takes both index
+
+// let b = "";
+// let a = [];
+
+// let a = false+true;
+
+// console.log(a)
+
+
+// if(true==1){
+//     console.log("hello")
+// }else{
+//     console.log("bye")
+// }
+
+
+// 2.push()
+
+// let arr = [1,5,9,3];
+
+// // console.log(arr.push(45));
+// let a = arr.push(45,12,89,"simran");
+// // let b = arr.push(10)
+
+// arr.pop();
+// arr.pop();
+// arr.pop();
+// console.log(arr);
+
+
+// let arr = [1,8,3,6];
+
+//   arr[1] = 10;
+//   arr[2] = 15
+//   console.log(arr)
+
+// let str = "sumit";
+//     str[0] = 'a';
+//     console.log(str)
+
+// let a = "hello";
+     
+//      let b  = a.concat("how are you")
+//       console.log(b);
+
+
+
+// 3. unshift() , shift()
+
+// let arr = [1,8,6,3];
+
+//  arr.unshift(4);
+//  arr.unshift(45,12,"kullu");
+//  arr.shift();
+//  arr.shift();
+
+//  console.log(arr);
+
+// 
+// 4.Array.isArray()
+
+//   let str = [1,"suman",12,78];
+//   let arr = "kullu";
+
+//   console.log(Array.isArray(str));
+
+//   if(Array.isArray(str)){
+//     console.log("ye array h")
+//   }else{
+//     console.log("array nahi h")
+//   }
+
+
+// 5.includes()
+
+// let a = [1,56,89,23];
+
+// console.log(a.includes(56))
+
+
+
+// 6.indexOf()
+
+// let arr = [1,8,6,55];
+
+// console.log(arr.indexOf(5));
+
+
+// 7**** slice()
+
+
+//  let arr = [10,20,30,40,50]
+
+// //  console.log(arr.slice());
+// // console.log(arr.slice(5));
+
+// console.log(arr.slice(-4,-2));
+
+
+
+
+// 8.**** join()
+
+let a = ["hello","how","are","you"];
+
+let b = "hello";
+console.log(b.split(""))
+
+  console.log(a.join(" "));
+
+// 9. splice()
+
+
+// let arr = [10,20,46,89,23];
+
+//       arr.splice(1,3,"kullu");
+
+//       console.log(arr);
+
+
+// 10 fill()
+
+  //  let arr = [10,20,30,56,44,89,78,54,22];
+  //  arr.fill(99,3,7);
+
+  //  console.log(arr)
+
+
+  // 11. reverse()
+
+    //  let arr = [10,2,20,89,56];
+    //  arr.reverse()
+    //  console.log(arr);
+
+    // 12. sort()
+
+    // let arr = [2,4,67,3,92,32,46];
+
+    //      arr.sort();
+    //      console.log(arr)
+
+    //      let str = ["aman","mannat","kullu","spiti","cat"]
+              
+    //           str.sort()
+    //           console.log(str)
+
+         
